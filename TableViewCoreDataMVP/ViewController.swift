57011c066dee6006bba3facef19d0e351647683c
sofiigorevna <sofia.konchencko@yandex.ru>
2023-11-26 19:18:21 +0300

    Initial Commit

---
+++ TableViewCoreDataMVP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewCoreDataMVP
+//
+//  Created by 1234 on 26.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
